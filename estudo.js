2b099140cb803f34f458fa3191ddd1b488cfa896
Felipe Burgdurf <felipeburgseelend@gmail.com>
2022-02-09 17:19:05 -0300

    curso de js

---
+++ estudo.js
@@ -0,0 +1,51 @@
+/*let idade = 5
+console.log(idade)
+let altura = 180
+console.log(altura)*/
+
+/*let valorIngressoAdulto = 150
+let valorIngressoInfantil = 75
+let valorIngressoBebe = 'Isento'
+console.log(valorIngressoAdulto)
+console.log(valorIngressoInfantil)
+console.log(valorIngressoBebe)*/
+
+/*let valorIngressoAdulto = 20;
+valorIngressoAdulto = 30;
+console.log(valorIngressoAdulto)*/
+
+/*let nome = 'Rafael';
+let idade = 25;
+let estaAprovado = true;
+let sobrenome = undefined;
+
+let pessoa = {
+    nome: 'Rafael',
+    idade: 25,
+    estaAprovado: true,
+    sobrenome: 'de Souza'
+};
+console.log(pessoa)
+
+let vamoDale = {
+    nome: 'Rei delas',
+    idade: 25,
+    estaAprovado: false,
+    sobrenome: 'Burgdurf'
+}
+console.log(vamoDale)*/
+
+//let familia = [true,45,'jhonatan',17];
+//console.log(familia.length)
+//console.log(familia[0]);
+
+//let namorada = ['Amanda', 17, //'Cachoeirinha', 'Meu amor']
+//console.log(namorada[3])
+
+let corSite = 'Azul'
+function resetaCor(cor, tonalidade){
+    corSite = cor + ' ' + tonalidade;
+};
+console.log(corSite);
+resetaCor('verde', 'claro')
+console.log(corSite);
